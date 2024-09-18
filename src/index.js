5003449c34191fc8e156b419e9b53246b51e6370
Fatima Hassan <110312999+FatimaHassan26@users.noreply.github.com>
2024-09-18 16:25:43 -0500

    Complet dropdown logic

--- src/index.js
+++ src/index.js
@@ -1,5 +1,30 @@
 import './style.css';
 import logo from './images/Famon_WEB.svg';
+import img1 from './images/charlesdeluvio-Mv9hjnEUHR4-unsplash.jpg'
+import workframe from './images/workframe.svg'
+import nature from './images/nature.svg'
+import dropdown from './images/dropdown_icon.svg'
 
 // Set the image source dynamically in the DOM
 document.getElementById('logo').src = logo;
+document.getElementById('image').src = nature;
+document.getElementById('dropdown-icon').src = dropdown;
+let frames = document.querySelectorAll('.workframe');
+frames.forEach(frame => {
+  frame.src = workframe;
+});
+document.querySelectorAll('.dropdown-button').forEach(button => {
+  button.addEventListener('click', function() {
+ 
+      const dropdownContent = this.nextElementSibling;
+  dropdownContent.style.display = dropdownContent.style.display === 'block' ? 'none' : 'block';
+});
+});
+
+window.addEventListener('click', function(event) {
+  if (!event.target.matches('.dropdown-button')) {
+      document.querySelectorAll('.dropdown-content').forEach(content => {
+          content.style.display = 'none';
+      });
+  }
+});
