b2cd8389e80904bd23b8d5445703c0a2507c1d12
Fatima Hassan <110312999+FatimaHassan26@users.noreply.github.com>
2024-09-20 12:50:45 -0500

    Add slideshow arrow

--- src/index.js
+++ src/index.js
@@ -4,11 +4,17 @@ import img1 from './images/charlesdeluvio-Mv9hjnEUHR4-unsplash.jpg'
 import workframe from './images/workframe.svg'
 import nature from './images/nature.svg'
 import dropdown from './images/dropdown_icon.svg'
+import backwardarrow from './images/arrow_back.svg'
+import forwardarrow from './images/arrow_forward.svg'
 
 // Set the image source dynamically in the DOM
 document.getElementById('logo').src = logo;
 document.getElementById('image').src = nature;
 document.getElementById('dropdown-icon').src = dropdown;
+document.getElementById('backward-arrow').src = backwardarrow;
+document.getElementById('forward-arrow').src = forwardarrow;
+document.getElementById('slide2').src = nature;
+document.getElementById('slide3').src = nature;
 let frames = document.querySelectorAll('.workframe');
 frames.forEach(frame => {
   frame.src = workframe;
